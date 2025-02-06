95371deef2ffce6762ef6f7c0a15e93231eb5418
SashaP-0 <phillipssasha02@gmail.com>
2025-02-06 12:32:50 +0000

    pathways

--- mainbooks.php
+++ mainbooks.php
@@ -8,7 +8,8 @@
         <li><a href="searchbooks.php">Search for a Book</a></li>
         <li><a href="managebooks_dropdown.php">Manage Books</a></li>
         <li><a href="addbooks.php">Add a New Book</a></li>
+</br>
+        <a href="index.php">Index</a>
     </ul>
 </body>
 </html>
-//
