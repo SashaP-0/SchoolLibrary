cbe65456f04efec0cea84621d127895a85daf969
Zeb5 <832.blackburn.zeb@gmail.com>
2025-02-13 12:54:05 +0000

    hmm

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>Home</title>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="library.css">
-    </head>
-    <body>
-        <h1>Library</h1>
-        <ul>
-            <li><a href="mainbooks.php">Books</a></li>
-            <li><a href="login.php">Login</a></li>
-            <li><a href="signup.php">Sign up</a></li>
-        </ul>
-    </body>
+<html>
+<head>
+</head>
+<body>
+    <ul>
+        <li><a href="mainbooks.php">Books</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="signup.php">Sign up</a></li>
+    </ul>
+</body>
 </html>
