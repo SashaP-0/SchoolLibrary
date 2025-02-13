bda0dd43f5ccd2ab9269164356a858232d23fe50
Zeb5 <832.blackburn.zeb@gmail.com>
2025-02-13 12:32:15 +0000

    Enhance index.php with HTML5 structure and meta tags

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
-<html>
-<head>
-</head>
-<body>
-    <ul>
-        <li><a href="mainbooks.php">Books</a></li>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="signup.php">Sign up</a></li>
-    </ul>
-</body>
+<html lang="en">
+    <head>
+        <title>Home</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="library.css">
+    </head>
+    <body>
+        <ul>
+            <li><a href="mainbooks.php">Books</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="signup.php">Sign up</a></li>
+        </ul>
+    </body>
 </html>
